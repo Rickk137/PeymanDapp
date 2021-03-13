673fb03d10741103d0e22939dfdda3f9cb12dd2f
peyman <peymannaderi3@yahoo.com>
2021-03-14 00:36:06 +0330

    config tailwind and add colors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      cta: "#FEC004",
+      input: "#2274A5",
+      tradingLong: "#17C3B2",
+      tradingShort: "#D74E09",
+      white: "#E9EAE9",
+      black: "#000000",
+      overlay: "#707070",
+      daapBackground: "#191919",
+      warningRed: "#A52222",
+    },
   },
   variants: {
     extend: {},
