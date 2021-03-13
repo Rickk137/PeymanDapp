a5550a9a298d611c36031f9721b4ddae2dc79a21
peyman <peymannaderi3@yahoo.com>
2021-03-13 23:03:02 +0330

    add weenusToken config.

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Web3 from "web3";
-
+const {
+  WEENUS_TOKEN_ADDRESS,
+  WEENUS_TOKEN_ABI,
+} = require("../../config/WeenusToken");
 declare global {
   interface Window {
     ethereum: any;
@@ -10,21 +13,45 @@ declare global {
 
 function Header() {
   const [wallet, setWallet] = useState("");
+  const [web3, setWeb3] = useState({} as Web3);
+  const [weenusToken, setWeenusToken] = useState(null);
 
   const ethEnabled = async () => {
-    if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
+    try {
+      if (window.ethereum) {
+        setWeb3(new Web3(window.ethereum));
+        if (web3) {
+          window.web3 = web3;
+          window.ethereum.enable();
 
-      window.web3 = web3;
-      window.ethereum.enable();
+          const network = await web3.eth.net.getNetworkType();
+          const accounts = await web3.eth.getAccounts();
+          if (accounts[0]) setWallet(accounts[0]);
 
-      const network = await web3.eth.net.getNetworkType();
-      const accounts = await web3.eth.getAccounts();
-      if (accounts[0]) setWallet(accounts[0]);
-      console.log("network:", network, accounts);
-      return true;
-    }
-    return false;
+          const weenusTokenObj = await new web3.eth.Contract(
+            WEENUS_TOKEN_ABI,
+            WEENUS_TOKEN_ADDRESS
+          );
+
+          //@ts-ignore
+          setWeenusToken(weenusTokenObj);
+          const balance = await weenusTokenObj.methods
+            .balanceOf(accounts[0])
+            .call();
+
+          console.log("balance:", balance);
+        }
+      }
+    } catch (error) {}
+  };
+
+  const transfer = async () => {
+    const accounts = await web3.eth.getAccounts();
+    await web3.eth.sendTransaction({
+      from: accounts[0],
+      to: WEENUS_TOKEN_ADDRESS,
+      value: "1",
+    });
   };
 
   useEffect(() => {
@@ -34,6 +61,7 @@ function Header() {
   return (
     <header>
       <button onClick={ethEnabled}> {wallet || "Engage Wallet"}</button>
+      <button onClick={transfer}>transfer</button>
     </header>
   );
 }
