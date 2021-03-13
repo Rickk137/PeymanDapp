f628045e0451be8209fc6e3c9ab91a31d86c1768
peyman <peymannaderi3@yahoo.com>
2021-03-13 17:48:04 +0330

    add header engage.

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -8,29 +8,32 @@ declare global {
   }
 }
 
-const ethEnabled = async () => {
-  if (window.ethereum) {
-    const web3 = new Web3(window.ethereum);
-
-    window.web3 = web3;
-    window.ethereum.enable();
-
-    const network = await web3.eth.net.getNetworkType();
-    const accounts = await web3.eth.getAccounts();
-    console.log("network:", network, accounts);
-    return true;
-  }
-  return false;
-};
-
 function Header() {
+  const [wallet, setWallet] = useState("");
+
+  const ethEnabled = async () => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+
+      window.web3 = web3;
+      window.ethereum.enable();
+
+      const network = await web3.eth.net.getNetworkType();
+      const accounts = await web3.eth.getAccounts();
+      if (accounts[0]) setWallet(accounts[0]);
+      console.log("network:", network, accounts);
+      return true;
+    }
+    return false;
+  };
+
   useEffect(() => {
     ethEnabled();
   }, []);
 
   return (
     <header>
-      <button>Engage Wallet</button>
+      <button onClick={ethEnabled}> {wallet || "Engage Wallet"}</button>
     </header>
   );
 }
