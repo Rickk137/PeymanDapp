5b17dc258f76ad6dc672f7e402eace80b7b6b6ec
peyman <peymannaderi3@yahoo.com>
2021-03-14 14:20:07 +0330

    fix tailwind issue.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      cta: "#FEC004",
-      input: "#2274A5",
-      tradingLong: "#17C3B2",
-      tradingShort: "#D74E09",
-      white: "#E9EAE9",
-      black: "#000000",
-      overlay: "#707070",
-      daapBackground: "#191919",
-      warningRed: "#A52222",
+      cta: '#FEC004',
+      input: '#2274A5',
+      tradingLong: '#17C3B2',
+      tradingShort: '#D74E09',
+      white: '#E9EAE9',
+      black: '#000000',
+      overlay: '#707070',
+      daapBackground: '#191919',
+      warningRed: '#A52222',
 
       gray: {
-        100: "#686868",
-        200: "#383838",
+        100: '#686868',
+        200: '#383838',
       },
     },
   },
   variants: {
-    extend: {},
+    opacity: ({ after }) => after(['disabled']),
   },
   plugins: [],
 };
